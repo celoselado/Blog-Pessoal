60d907f8ac8769cdfd1c2c642d94799056fb7a25
unknown <malexandresantos@hotmail.com>
2023-05-15 16:03:06 -0300

    Criação NavBar, Footer e Home

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
